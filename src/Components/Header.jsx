137f5d36021d607211e8700d56a7097a31b69571
Sonay Kara <zonaykara@gmail.com>
2026-04-08 19:12:37 +0300

    updated header image path

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,10 +5,10 @@ const Header = () => {
     <div className="">
       <header className=" bg-gray-600 h-auto p-5 flex justify-between items-center rounded-2xl">
         <div>
-          <img src="../../public/header/logo.svg"></img>
+          <img src="/header/logo.svg"></img>
         </div>
         <div className="p-3 bg-gray-400 rounded-xl">
-          <img src="../../public/header/icon-sun.svg"></img>
+          <img src="/header/icon-sun.svg"></img>
         </div>
       </header>
     </div>
